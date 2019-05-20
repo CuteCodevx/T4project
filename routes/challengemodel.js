cc5f49e1c1e5975849c8e6807288d1e2b9eae141
firedancee <38534715+firedancee@users.noreply.github.com>
2019-05-20 23:15:53 +0100

    connection fixing

--- routes/challengemodel.js
+++ routes/challengemodel.js
@@ -1,8 +1,8 @@
 // import mongoose package
 var mongoose=require('mongoose');
 var Schema=mongoose.Schema;
-// connec the database
-mongoose.connect("mongodb+srv://admin:123@cluster0-mozuc.mongodb.net/test?retryWrites=true", {dbName: 'mongoose'});
+// connect the database
+mongoose.connect('mongodb+srv://team4:team4123456@cluster0-mozuc.mongodb.net/test?retryWrites=true', {dbName: 'mongoose'});
 // set the schema for the form
 var newSchema = new Schema({
     index:[Number],

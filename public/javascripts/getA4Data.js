3ba8d38d79d7c2c24a0d259595ff4b474ad90033
crystalQin <qinfanhua2015@163.com>
2019-05-21 14:22:48 +0100

    add comments in two js files in public javascripts folder.

--- public/javascripts/getA4Data.js
+++ public/javascripts/getA4Data.js
@@ -1,16 +1,21 @@
+/**
+ * This file is used for competition management.
+ */
 $(document).ready(function() {
 
-
-
+    /**
+     * when the admin click to stop one specific challenge, the name of the challenge will be transfered to route,
+     * and then this challenge will be stopped accrodingly.
+     */
     $('#stopChallengeForm').on('submit', function(event) {
 
         event.preventDefault();
 
         var d={};
-        //button is 1 in stopChallengeForm
+
+        //button equals one means one specific challenge will be stopped.
         d['button']='1';
 
-        // [{name: "a1", value: "xx"},{name: "a2", value: "xx"}],
         var wholeForm = $('#stopChallengeForm').serializeArray();
 
         $.each(wholeForm, function() {
@@ -29,13 +34,16 @@ $(document).ready(function() {
     });
 
 
+    /**
+     * This function is used to change the challenge timetable.
+     */
     $('#challengeTimetableForm').on('submit', function(event) {
         event.preventDefault();
         var d=$(this).serialize();
         $.ajax({
             url:'/a4',
             type:'post',
-            data:d+'&'+'button=2',
+            data:d+'&'+'button=2',//button equals two means the timetable of the challenge will be changed.
             dataType:'json',
             success:function () {
                 window.alert("Successfully updated!");
--- public/javascripts/getFormData.js
+++ public/javascripts/getFormData.js
@@ -1,17 +1,29 @@
+/**
+ * This file is used for both login function and challenges with two flexible method options.
+ */
 $(document).ready(function() {
 
-
+    /**
+     * if the first method has been chosen, only the details of the first method would be shown accordingly.
+     */
     $('#methodOption1').on('click',function(event){
         $('#method1Div').show();
         $('#method2Div').hide();
     });
 
+    /**
+     * if the second method has been chosen, only the details of the second method would be shown accordingly.
+     */
     $('#methodOption2').on('click',function(event){
         $('#method2Div').show();
         $('#method1Div').hide();
     });
 
     
+    /**
+     * this function with ajax will transfer the username and password to the route.
+     * The web page will turn to challenge or admin page according to the role of the user.
+     */
     $('#login').on('submit',function(e){
         e.preventDefault();
         var loginInfo={};
@@ -24,13 +36,13 @@ $(document).ready(function() {
             data: loginInfo,
             dataType: 'json',
             success: function (data) {
-              var err = data.err
-              if (err === 0) {
+              var err = data.err;
+              if (err === 0) { // invalid username or password.
                   $("#invalidAlert").show();
               }else{
-                  if(data.level==3)
+                  if(data.level==3) // the user is the admin.
                       window.location.href='/a1';
-                  else
+                  else // the user is the judge.
                       window.location.href='/c1';
               }
             }
